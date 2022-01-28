58b929c7819bc1ef53d840b36d0c3ef3a2ad4f0c
anaismiraira <mirairaanais@gmail.com>
2022-01-28 11:57:25 -0400

    1157 28012022 unitsII js

--- assets/js/index.js
+++ assets/js/index.js
@@ -11,3 +11,24 @@ if (screen.width > 768){
 }
 
 new WOW({offset:offset, scrollContainer: null}).init()
+
+// ------------------------------img-edf-units----------------------------
+
+function click(id) {
+  $(".activeLevel").removeClass("activeLevel");
+  $(".activeText").removeClass("activeText");
+  setTimeout(() => {
+    $(`#${id}`).addClass("activeLevel");
+    $(`.${id}`).addClass("activeText");
+    $("#changeImg").attr("src",`assets/images/unidades/plantas/${id}.png`)
+    $("#changeImgUrl").attr("href",`assets/images/uniddes/plantas/${id}.png`)
+  },10)
+}
+
+$(document).ready(function() {
+  $("#changeImgUrl").fancybox({
+    overlay : {
+      closeClick : true,
+    }
+  });
+})
