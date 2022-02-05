f8185845c3cb4484fa79425738bfc82b40f1af8b
anaismiraira <mirairaanais@gmail.com>
2022-02-05 11:21:41 -0400

    1121 05022022 mobil

--- assets/js/index.js
+++ assets/js/index.js
@@ -117,7 +117,7 @@ $("#lightgallery .swiper-wrapper").lightGallery();
 // ------------------------------Hover, Active Location-----------------------------
 $(".ulist .list").hover(function(){
   let type = $(this).data("type")
-  $(`.${type}`).css("opacity", "1");
+  $(`.${type}`).css("opacity", "1")
   }, function(){
   let type = $(this).data("type")
   $(`.${type}`).css("opacity", "0");
@@ -126,9 +126,10 @@ $(".ulist .list").hover(function(){
 $(".ulist .list").click(function(){
   let type = $(this).data("type")
   $(`.${type}`).addClass("active").siblings().removeClass("active")
+  $(this).addClass("selector")
   });
 
 $(".mybtn").click(function(){
-  $(".mlocation div").addClass("active")
+  $(".mlocation div").addClass("active").siblings().removeClass("selector")
 });
 
